e3f4b9282a8c3784cc6d0e084c386ef9c2ada327
Levan Sturua <agmashenebel@gmail.com>
2017-05-22 16:09:10 +0300

    Create Sarathstra.sql
    logic behind Sarathasa SAI
    Adding spells, e.t.c

---
+++ Sarathstra.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO `world`.`smart_scripts` (`entryorguid`, `id`, `event_param1`, `event_param2`, `event_param3`,
+ `event_param4`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES ('26858', '8', '5000',
+ '8000', '6000', '9000', '11', '47425', '2', 'Frost Breath');
+
+INSERT INTO `world`.`smart_scripts` (`entryorguid`, `id`, `event_param1`, `event_param2`, `event_param3`,
+ `event_param4`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES ('26858', '9', '15000',
+ '18000', '40000', '43000', '11', '61085', '5', 'blizzard');
+
+INSERT INTO `world`.`smart_scripts` (`entryorguid`, `id`, `event_param1`, `event_param2`, `event_param3`, 
+`event_param4`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES ('26858', '10', '45000',
+ '48000', '45000', '48000', '11', '39005', '1', 'Shadow Nova');
