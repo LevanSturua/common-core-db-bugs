2b2f468b72d415d9564835917e9940b5680aeaa4
Levan Sturua <agmashenebel@gmail.com>
2017-06-01 01:06:33 +0300

    Create realm_first_titles.sql

---
+++ realm_first_titles.sql
@@ -0,0 +1,80 @@
+-- @levan_sturua_realm_first_titles
+-- general
+-- realm first level 80
+
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('457', '85', '85');
+
+-- classes
+-- realm first 80 orc
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1410', '0', '116');
+
+-- realm first 80 troll
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1412', '0', '118');
+-- realm first 80 blood elf
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1405', '0', '110');
+-- realm first 80 tauren
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1411', '0', '117');
+-- realm first 80 undead
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1413', '0', '119');
+-- realm first 80 human
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1408', '114', '0');
+-- realm first 80 night elf
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1409', '115', '0');
+--  realm first 80 dwarf
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1407', '112', '0');
+-- realm first 80 gnome
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1404', '113', '0');
+-- realm first 80 draenei
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1406', '111', '0');
+
+-- classes
+-- realm first 80 mage
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('460', '93', '93');
+-- realm first 80 priest
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('464', '89', '89');
+-- realm first 80 warlock
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('463', '90', '90');
+-- realm first 80 druid
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('466', '87', '87');
+-- realm first 80 rogue
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('458', '95', '95');
+-- realm first 80 shaman
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('467', '86', '86');
+-- realm first 80 hunter
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('462', '91', '91');
+-- realm first 80 paladin
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('465', '156', '156');
+-- realm first 80 death knight
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('461', '92', '92');
+-- realm first 80 warrior
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('459', '94', '94');
+
+-- professions
+-- realm first 450 alchemy
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1415', '96', '96');
+-- realm first 450 blacksmith
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1414', '97', '97');
+-- realm first 450 cooking
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1416', '98', '98');
+-- realm first 450 enchanting 
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1417', '99', '99');
+-- realm firt 450 engineering 
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1418', '100', '100');
+-- realm first 450 first aid
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1419', '101', '101');
+-- realm first 450 fishing
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1420', '102', '102');
+-- realm first 450 herbalism
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1421', '103', '103');
+-- realm first 450 inscription
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1422', '104', '104');
+-- realm first 450 jewelcrafting
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1423', '105', '105');
+-- realm first 450 leatherworking
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1424', '106', '106');
+-- realm first 450 mining
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1425', '107', '107');
+-- realm first 450 skinning
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1426', '108', '108');
+-- realm first 450 tialoring
+INSERT INTO `world`.`achievement_reward` (`entry`, `title_A`, `title_H`) VALUES ('1427', '109', '109');
